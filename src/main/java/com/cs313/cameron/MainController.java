a551d653d91743356ef742a9cba5076c02762d04
cameronwlewis <cameronwlewis@gmail.com>
2017-04-07 21:01:30 -0600

    more refactoring

--- src/main/java/com/cs313/cameron/MainController.java
+++ src/main/java/com/cs313/cameron/MainController.java
@@ -4,11 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by cameronlewis on 4/7/17.
- */
 @Controller
-public class TestController {
+public class MainController {
     @RequestMapping(value = "/")
     @ResponseBody
     public String greeting() {
