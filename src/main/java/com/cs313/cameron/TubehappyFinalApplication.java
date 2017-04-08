a569a62239229a8842df100d2e713e0faa4927c1
cameronwlewis <cameronwlewis@gmail.com>
2017-04-07 20:59:51 -0600

    refactored some stuff

--- src/main/java/com/cs313/cameron/TubehappyFinalApplication.java
+++ src/main/java/com/cs313/cameron/TubehappyFinalApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TubehappyFinalApplication {
+public class TubeHappyFinalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TubehappyFinalApplication.class, args);
+		SpringApplication.run(TubeHappyFinalApplication.class, args);
 	}
 }
