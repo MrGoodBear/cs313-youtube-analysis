bc99bb96acfb91185231dc2885bb22d4489245b7
cameronwlewis <cameronwlewis@gmail.com>
2017-04-07 21:14:19 -0600

    fixed some small bugs

--- src/main/java/com/cs313/cameron/ServletInitializer.java
+++ src/main/java/com/cs313/cameron/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TubehappyFinalApplication.class);
+		return application.sources(TubeHappyFinalApplication.class);
 	}
 
 }
