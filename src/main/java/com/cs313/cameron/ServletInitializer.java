5f8f021b3fe3f897d88115d0c833f0bb266a3211
cameronwlewis <cameronwlewis@gmail.com>
2017-04-07 20:56:13 -0600

    initial commit. And there was much rejoicing

---
+++ src/main/java/com/cs313/cameron/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cs313.cameron;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TubehappyFinalApplication.class);
+	}
+
+}
