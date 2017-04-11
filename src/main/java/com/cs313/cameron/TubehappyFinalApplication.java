9b08ffa511488a3caa927e27c174415fd52a4b6d
cameronwlewis <cameronwlewis@gmail.com>
2017-04-11 17:31:12 -0600

    uclassify_Formatter working great now

--- src/main/java/com/cs313/cameron/TubehappyFinalApplication.java
+++ src/main/java/com/cs313/cameron/TubehappyFinalApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Where the main application is directed and run.
@@ -15,6 +17,8 @@ public class TubeHappyFinalApplication {
 	public static void main(String[] args) throws FileNotFoundException {
 		SpringApplication.run(TubeHappyFinalApplication.class, args);
         Formatter formatter = new Formatter();
-        formatter.uclassify_Formatter();
+        List<String> text = Arrays.asList("i like turtles", "baby girl, I like your moves", "hey big boy");
+
+        formatter.uclassify_Formatter(text, text.size());
 	}
 }
--- src/main/java/com/cs313/cameron/model/Formatter.java
+++ src/main/java/com/cs313/cameron/model/Formatter.java
@@ -6,52 +6,60 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Properties;
 
 /** Responsible for formatting URL queries for API calls, and converting XML to JSON. */
 public class Formatter {
 
-    private int uclassify_FileCount;
-    private String api_key;
 
+    private int uclassify_FileCount;;
+    private String api_key;
     public Formatter() {
-        this.uclassify_FileCount = 1;
+        this.uclassify_FileCount = 0;
         this.api_key = "tGv51rglTt5D";
     }
 
 
-    public void convertXMLtoJSON() {
-  };
+public void convertXMLtoJSON() {
+  }
 
-  public File uclassify_Formatter(String textContent) throws FileNotFoundException {
-      int textCount = 1;
-      int classifyCount = 1;
+  public File uclassify_Formatter(List<String> textContent, int commentCount) throws FileNotFoundException {
       String api_key = "tGv51rglTt5D";
-      String fileName = "/uClassify_file" + uclassify_FileCount + "xml";
+      String fileName = "uClassify_file" + uclassify_FileCount + ".xml";
 
-      XMLBuilder2 builder = XMLBuilder2.create("uclassify")
-              .attribute("version", "1.01")
-              .attribute("xmlns", "http://api.uclassify.com/1/RequestSchema")
-              .element("texts")
-              .element("text").attribute("id", "text_" + textCount).text(textContent)
-              // add new texts to batch right here
-              .up()
-              .up()
-              .element("readCalls").attribute("readApiKey", api_key)
-              .element("classify").attribute("id", "call_" + classifyCount).attribute("username", "uClassify")
-              .attribute("classifierName", "Sentiment").attribute("textID", "text_" + textCount)
-              .up()
-              .up();
+          XMLBuilder2 builder = XMLBuilder2.create("uclassify")
+                  .attribute("version", "1.01")
+                  .attribute("xmlns", "http://api.uclassify.com/1/RequestSchema")
+                  .element("texts");
+      for (int i = 0; i < commentCount; i++) {
+          builder.xpathFind("//texts").element("text").attribute("id", "text_" + i).text(textContent.get(i));
+          //maybe a foreach?
+          // add new texts to batch right here
+      }
+      builder.xpathFind("//texts").up().up()
+        .element("readCalls").attribute("readApiKey", api_key);
 
+      for (int i = 0; i < commentCount; i++) {
+          builder.xpathFind("//readCalls")
+                  .element("classify").attribute("id", "call_" + i).attribute("username", "uClassify")
+                  .attribute("classifierName", "Sentiment").attribute("textId", "text_" + i);
+      }
+      builder.xpathFind("//readCalls").up().up();
+
+      // create new file to store calls
+      File file = new File(fileName);
       Properties outputProperties = new Properties();
+
       // Explicitly identify the output as an XML document
       outputProperties.put(javax.xml.transform.OutputKeys.METHOD, "xml");
       // Pretty-print the XML output (doesn't work in all cases)
       outputProperties.put(javax.xml.transform.OutputKeys.INDENT, "yes");
-      File file = new File(fileName);
-      PrintWriter writer = new PrintWriter(new FileOutputStream("projects.xml"));
+
+      PrintWriter writer = new PrintWriter(new FileOutputStream(fileName));
       builder.toWriter(writer, outputProperties);
 
+      uclassify_FileCount++;
       return file;
   };
 
